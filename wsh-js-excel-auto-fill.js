8ee3ed711de72ae45af19a8ae5a9b0c856b3c007
sworc <lightbox@nifty.com>
2022-05-07 18:20:24 +0900

    Add files via upload

--- wsh-js-excel-auto-fill.js
+++ wsh-js-excel-auto-fill.js
@@ -12,48 +12,48 @@ ExcelApp.DisplayAlerts = false;
 
 try {
 
-	// ****************************
-	// ブック追加
-	// ****************************
-	var Book = ExcelApp.Workbooks.Add();
+    // ****************************
+    // ブック追加
+    // ****************************
+    var Book = ExcelApp.Workbooks.Add();
 
-	// 通常一つのシートが作成されています
-	var Sheet = Book.Worksheets( 1 );
+    // 通常一つのシートが作成されています
+    var Sheet = Book.Worksheets( 1 );
 
-	// ****************************
-	// シート名変更
-	// ****************************
-	Sheet.Name = "JScriptの処理";
+    // ****************************
+    // シート名変更
+    // ****************************
+    Sheet.Name = "JScriptの処理";
 
-	// ****************************
-	// セルに値を直接セット
-	// ****************************
-	for( var i = 1; i <= 10; i++ )
-	{
-		Sheet.Cells(i, 1) = "処理 : " + i;
-	}
+    // ****************************
+    // セルに値を直接セット
+    // ****************************
+    for( var i = 1; i <= 10; i++ )
+    {
+        Sheet.Cells(i, 1) = "処理 : " + i;
+    }
 
-	// ****************************
-	// 1つのセルから
-	// AutoFill で値をセット
-	// ****************************
-	Sheet.Cells(1, 2) = "子";
-	// 基となるセル範囲
-	var SourceRange = Sheet.Range(Sheet.Cells(1, 2), Sheet.Cells(1,2));
-	// オートフィルの範囲(基となるセル範囲を含む )
-	var FillRange = Sheet.Range(Sheet.Cells(1, 2), Sheet.Cells(10,2));
-	SourceRange.AutoFill(FillRange);
+    // ****************************
+    // 1つのセルから
+    // AutoFill で値をセット
+    // ****************************
+    Sheet.Cells(1, 2) = "子";
+    // 基となるセル範囲
+    var SourceRange = Sheet.Range(Sheet.Cells(1, 2), Sheet.Cells(1,2));
+    // オートフィルの範囲(基となるセル範囲を含む )
+    var FillRange = Sheet.Range(Sheet.Cells(1, 2), Sheet.Cells(10,2));
+    SourceRange.AutoFill(FillRange);
 
-	// ****************************
-	// 保存
-	// ****************************
-	Book.SaveAs( WshShell.CurrentDirectory + "\\sample.xlsx" );
- 
+    // ****************************
+    // 保存
+    // ****************************
+    Book.SaveAs( WshShell.CurrentDirectory + "\\sample.xlsx" );
+
 } catch (error) {
-	ExcelApp.Quit();
-	ExcelApp = null;
-	WshShell.Popup(error.description);
-	WScript.Quit();	
+    ExcelApp.Quit();
+    ExcelApp = null;
+    WshShell.Popup(error.description);
+    WScript.Quit();	
 }
 
 ExcelApp.Quit();
